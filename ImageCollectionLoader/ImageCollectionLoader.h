301ee6b051fbab4e4007ad3e192a3b7bca5f8a70
ohassan <ohassan443@gmail.com>
2019-10-19 18:49:52 +0200

    Initial Commit

---
+++ ImageCollectionLoader/ImageCollectionLoader.h
@@ -0,0 +1,19 @@
+//
+//  ImageCollectionLoader.h
+//  ImageCollectionLoader
+//
+//  Created by Omar Hassan  on 10/19/19.
+//  Copyright © 2019 Omar Hassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCollectionLoader.
+FOUNDATION_EXPORT double ImageCollectionLoaderVersionNumber;
+
+//! Project version string for ImageCollectionLoader.
+FOUNDATION_EXPORT const unsigned char ImageCollectionLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageCollectionLoader/PublicHeader.h>
+
+
