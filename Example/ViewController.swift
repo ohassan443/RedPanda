7f95eecfde2d4abc809e830e6993b767d3206157
ohassan <ohassan443@gmail.com>
2019-10-29 18:03:09 +0200

    temp

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tv2: UITableView!
     
     var dataSource = [String]()
-    let imageCollectionLoader = ImageCollectionLoaderBuilder()
+    let imageCollectionLoader = ImageCollectionLoaderBuilder().defaultImp()
     var session : URLSession? = nil
     
     override func viewDidLoad() {
@@ -22,34 +22,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
      
+//
+//        var dict = Dictionary<String, Any>()
+//
+//        dict["username"] = "ohassan@noor.net"
+//        dict["password"] = "qwerty"
+//        dict["grant_type"] = "password"
+//        let body = try! JSONSerialization.data(withJSONObject: dict, options: [])
+//
+//
+//
+//        var request = URLRequest(url: URL(string: "http://[::1]:8080/token")! as URL)
+//        request.httpBody = body
+//        request.httpMethod = "POST"
+//        request.addValue("application/json",forHTTPHeaderField: "Content-Type")
+//        request.addValue("application/json",forHTTPHeaderField: "Accept")
+//
+//        let firstConfig = URLSessionConfiguration.default
+//        firstConfig.timeoutIntervalForRequest = 30
+//        firstConfig.httpAdditionalHeaders = ["Authorization" : "Basic aW9zYXBwOmlvc2FwcF9wYXNzMTIzIQ=="]
+//        session = URLSession(configuration: firstConfig)
+//
+//        session!.dataTask(with: request){data,response,error in
+//            if let error = error {
+//                print(error)
+//            }
+//        }.resume()
         
-        var dict = Dictionary<String, Any>()
-        
-        dict["username"] = "ohassan@noor.net"
-        dict["password"] = "qwerty"
-        dict["grant_type"] = "password"
-        let body = try! JSONSerialization.data(withJSONObject: dict, options: [])
-        
-        
-        
-        var request = URLRequest(url: URL(string: "http://[::1]:8080/token")! as URL)
-        request.httpBody = body
-        request.httpMethod = "POST"
-        request.addValue("application/json",forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json",forHTTPHeaderField: "Accept")
-        
-        let firstConfig = URLSessionConfiguration.default
-        firstConfig.timeoutIntervalForRequest = 30
-        firstConfig.httpAdditionalHeaders = ["Authorization" : "Basic aW9zYXBwOmlvc2FwcF9wYXNzMTIzIQ=="]
-        session = URLSession(configuration: firstConfig)
-        
-        session!.dataTask(with: request){data,response,error in
-            if let error = error {
-                print(error)
-            }
-        }.resume()
-        
-        let tvs = [tv1,tv2]
+        let tvs = [tv1]
         tvs.forEach({
             $0?.delegate = self
             $0?.dataSource = self
@@ -72,7 +72,17 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! cell
+        let element = dataSource[indexPath.row]
         
+        if let card = imageCollectionLoader.cacheQueryState(url: element).image{
+			cell.set(image: card)
+        }else {
+            imageCollectionLoader.requestImage(requestDate: Date(), url: element, indexPath: indexPath, tag: "card", successHandler: {
+                image , index , date in
+                guard let visibleCell = tableView.cellForRow(at: index) as? cell else {return}
+                visibleCell.set(image: image)
+            })
+        }
         
         
         return cell
